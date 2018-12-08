3f72039ea2bea1220d94a8cfc61a92bfb2da5c3e
Iwan Jeriatno <jeriat01@gmail.com>
2018-12-09 07:34:30 +0800

    file script js

---
+++ script.js
@@ -0,0 +1,24 @@
+<script src="{{ asset('select2.min.js') }}"></script>
+<script type="text/javascript">
+  $(document).ready(function(){
+      $('#items').select2({
+          placeholder: 'Cari Barang',
+          ajax: {
+            url: "{{ route('barang.item') }}",
+            dataType: 'json',
+            delay: 250,
+            processResults: function (data) {
+              return {
+                results:  $.map(data, function (item) {
+                      return {
+                          text: item.item_code,
+                          id: item.id
+                      }
+                  })
+              };
+            },
+            cache: true
+          }
+      });
+  });
+</script>
