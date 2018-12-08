927a4c24eac130c029667a8cdb2905992482594a
Iwan Jeriatno <jeriat01@gmail.com>
2018-12-09 07:45:40 +0800

    file index html

--- index.blade.php
+++ index.blade.php
@@ -1,1 +1,1 @@
-<select class="form-control1 select2" ng-model="outlet_id" autofocus="on" ng-change="outlet()" tabindex="1" id="items"></select>
+<select class="form-control1 select2" tabindex="1" id="items"></select>
